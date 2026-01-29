a4f3e4252ad717f6bc76060e9b8a99a9c6e81e95
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-29 23:52:00 +0100

    Create SimplifiedTextScreen.kt

---
+++ SimplifiedTextScreen.kt
@@ -0,0 +1,50 @@
+package com.emmaxtech.documentsimplifier.ui
+
+import androidx.compose.foundation.layout 
+import androidx.compose.material
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@ param
+@ param
+
+@Composable 
+
+fun SimplifiedTextScreen(
+  simplifiedText
+  onRefresh: (() -> Unit)? = null
+  ) {
+
+  Scaffold(
+    topBar = {
+      TopAppBar(
+       title = { Text("Simplified Document")},
+     actions = {
+        if (onRefresh ! = null)
+          IconButton(onClick = onRefresh) {
+            Icon(
+              imageVector = Icons.Default.Refresh,
+              contentDescription = "Refresh"
+              )
+          }
+     }
+        }
+        )
+    }
+    { paddingValues ->
+      Column(
+        modifier = Modifier
+           .padding(paddingValues)
+           .padding(16.dp)
+           .fillMaxSize()
+        ) {
+        if (simplifiedText.isEmpty()) {
+          Text(
+            text = simplifiedText,
+            style = MaterialTheme.typography.body1
+            )
+        }
+      }
+    }
+    }
