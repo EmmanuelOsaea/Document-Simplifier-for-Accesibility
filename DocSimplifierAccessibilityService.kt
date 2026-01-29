f8cf584cef53ed175bf240d8e00e8a0f7c7b67d0
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-29 22:06:10 +0100

    Update and rename DocumentSimplifierAccesibiltyService.kt to DocSimplifierAccessibilityService.kt

---
+++ DocSimplifierAccessibilityService.kt
@@ -0,0 +1,27 @@
+import android accessibilityservice.AccessiblityService
+
+import android view.accessibility.AccessibilityEvent
+import android view.accessiblityAccessibilityNodeInfo
+
+class DocumentSimplifierAccessibilityService : AccessibilityService() {
+  override fun onAccessibilityEvent (event AccessibilityEvent?) {
+(event == null) return
+
+   if (event.eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED ||
+       event.eventType == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED) {
+     
+    val rootnode = rootInActive ?: return
+   
+    val extractedText = extractTextfromNode(rootnode)
+     //Display simplified text
+    val simplifiedText = simplifiedText(extractedText)
+     //Display simple  UI
+    showSimplifiedText(simplifiedText)
+   }
+  }
+
+  override fun onInterrupt() {
+    //
+  }
+
+  
--- DocumentSimplifierAccesibiltyService.kt
+++
@@ -1,3 +0,0 @@
-import android accessibility service.Accessiblity service
-.
-import android accessibility service.Accessiblity Evebt
