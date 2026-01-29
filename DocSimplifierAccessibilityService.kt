0a8e1dc2f57986efa93ac123268b53e41df9839a
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-29 22:39:52 +0100

    Update DocSimplifierAccessibilityService.kt

--- DocSimplifierAccessibilityService.kt
+++ DocSimplifierAccessibilityService.kt
@@ -24,4 +24,27 @@ class DocumentSimplifierAccessibilityService : AccessibilityService() {
     //
   }
 
+  private fun extractTextFromNode(node: AccessibilityNodeInfo):
+    val sb = StringBuilder()
+    fun recurse(n: Accessibility node info?) {
+          if (n == null) return
+        if (n.text != null) {
+       sb.append(n.text).append("")
+        }
+         
+      
+      
+      
+      
+      
+      private fun simplifyText (text: String): String {
   
+  
+    return text
+  }
+    
+    private fun showSimplifiedText(text: String) {
+
+
+}
+}
