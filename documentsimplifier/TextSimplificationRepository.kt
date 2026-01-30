b401748f87282236b2591ebe127a24c651f3c471
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-30 15:52:05 +0100

    Create TextSimplificationRepository.kt

---
+++ documentsimplifier/TextSimplificationRepository.kt
@@ -0,0 +1,9 @@
+package com.emmaxtech.documentsimplifier.data
+
+class TextSimplificationRepository {
+
+  fun simplifyText
+
+  return "$originalText\n\n(Simplified at ${System.currentTimeMillis()})"
+}
+}
