6b010616bcf3df93e23420e24c067b58b90a1491
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-30 15:27:19 +0100

    Update SimplifiedTextScreen.kt

--- documentsimplifier/SimplifiedTextScreen.kt
+++ documentsimplifier/SimplifiedTextScreen.kt
@@ -1,8 +1,11 @@
 package com.emmaxtech.documentsimplifier.ui
 
-import androidx.compose.foundation.layout 
-import androidx.compose.material
+import androidx.compose.foundation.layout*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -41,12 +44,12 @@ fun SimplifiedTextScreen(
         ) {
         if (simplifiedText.isEmpty()) {
           Text(
-            text = simplifiedText,
+            text = "No simplified text available.",
             style = MaterialTheme.typography.body1
             )
         } else
         Text(
-          text = simplifiedText
+          text = simplifiedText,
           style = MaterialTheme.typography.body1
          )
     }
