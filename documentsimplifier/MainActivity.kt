845df7029aa7514f4cf73afd21ef4fffeff7bb19
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-01-30 15:12:20 +0100

    Update MainActivity.kt

--- documentsimplifier/MainActivity.kt
+++ documentsimplifier/MainActivity.kt
@@ -1,3 +1,31 @@
+import android.os.bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate
+     setContent {
+       val viewmodel = androidx.lifecycle.viewmodel.compose.viewmodel<simplifiedTextViewModel>()
+       
+       LaunchedEffect(Unit) {
+         viewmodel.setOriginalText("This is a arduous text that needs to be simplified")
+       }
+       SimplifiedTextScreenWithViewModel = viewModel)
+     }
+  }
+}
+
+
+
+
+
+
+
+
+
+
 setContent {
   var simplifiedText by remember { mutableStateOf("Hello! Simplified text will display here.")}
 
